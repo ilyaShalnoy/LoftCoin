e1afcb90f5f2eab31fb2eeb045dce64598d2d10b
ilyaShalnoy <ilya.shalnov.97@gmail.com>
2021-05-30 14:48:51 +0300

    + WalletsFragment UI

--- app/src/main/java/com/example/loftcoin/ui/main/MainActivity.java
+++ app/src/main/java/com/example/loftcoin/ui/main/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.loftcoin.ui.main;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
@@ -16,5 +20,10 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(binding.toolbar);
         setContentView(binding.getRoot());
 
+        final NavController navController = Navigation.findNavController(this, R.id.main_host);
+        NavigationUI.setupWithNavController(binding.bottomNav, navController);
+        NavigationUI.setupWithNavController(binding.toolbar, navController, new AppBarConfiguration
+                .Builder(binding.bottomNav.getMenu())
+                .build());
     }
 }
--- app/src/main/java/com/example/loftcoin/ui/wallets/WalletsFragment.java
+++ app/src/main/java/com/example/loftcoin/ui/wallets/WalletsFragment.java
@@ -1,4 +1,49 @@
 package com.example.loftcoin.ui.wallets;
 
-public class WalletsFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.PagerSnapHelper;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.SnapHelper;
+
+import com.example.loftcoin.R;
+import com.example.loftcoin.databinding.FragmentWalletsBinding;
+
+import org.jetbrains.annotations.NotNull;
+
+public class WalletsFragment extends Fragment {
+
+    private SnapHelper walletsSnapHelper;
+
+    @Nullable
+    @org.jetbrains.annotations.Nullable
+    @Override
+    public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_wallets, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        final FragmentWalletsBinding binding = FragmentWalletsBinding.bind(view); //TODO: ?
+        walletsSnapHelper = new PagerSnapHelper();
+        walletsSnapHelper.attachToRecyclerView(binding.recycler);
+        binding.recycler.setLayoutManager(new LinearLayoutManager(view.getContext(), RecyclerView.HORIZONTAL, false));
+        binding.recycler.setAdapter(new WalletsAdapter());
+        binding.recycler.setVisibility(View.VISIBLE);
+        binding.addWalletCard.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onDestroyView() {
+        walletsSnapHelper.attachToRecyclerView(null);
+        super.onDestroyView();
+    }
 }
